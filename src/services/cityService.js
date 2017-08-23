fa59994d0cda11178a85da6bedde6926babb0d84
unknown <946163699@qq.com>
2017-08-23 17:51:47 +0800

    更新了城市数据获取

--- src/services/cityService.js
+++ src/services/cityService.js
@@ -6,8 +6,20 @@ function getCityData(){
 	return new Promise((resolve,reject)=>{
 		axios.get(`${API.CityData}?_t=${new Date().getTime()}`)
 		.then((res)=>{
-			var arr={}
-			console.log(res.data.data.cities)
+			var arr=["A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","W","X","Y","Z"]
+			var newArr=[]
+			arr.map((item)=>{
+				var obj = {}
+				obj[item] = []
+				res.data.data.cities.map((pinyin)=>{
+					var str = pinyin.pinyin.charAt(0).toUpperCase()
+					if(item == str){
+						obj[item].push(pinyin)
+					}
+				})
+				newArr.push(obj)
+			})
+			console.log(newArr)
 		})
 		.catch((error)=>{
 			console.log(error)
